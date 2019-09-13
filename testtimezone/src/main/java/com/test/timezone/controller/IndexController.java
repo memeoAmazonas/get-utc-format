1139564163b6aecb6ef4cebc1e70699aceda863b
Jose Ortiz <jortiz@mememo.com>
2019-09-13 05:54:05 -0400

    finally commit

--- testtimezone/src/main/java/com/test/timezone/controller/IndexController.java
+++ testtimezone/src/main/java/com/test/timezone/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.test.timezone.controller;
 
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.test.timezone.services.TimezoneServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,12 @@ public class IndexController {
 
     @Autowired
     private TimezoneServices timezoneServices;
-
+    @CrossOrigin
     @PostMapping(value="/get")
-    public String getTimeZone(@RequestParam String hour, @RequestParam String timezone) {
-        return timezoneServices.getTimeZone(hour,timezone);
+    public String getTimeZone( @RequestBody String requestJson) {
+        JsonObject object = new JsonParser().parse(requestJson).getAsJsonObject();
+        //@RequestParam String hour, @RequestParam String timezone
+
+        return timezoneServices.getTimeZone(object.get("hour").getAsString(),object.get("timezone").getAsString());
     }
 }
